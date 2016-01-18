2befa9139ee15f30734a7ec48664e6ef119779f2
Josh Anderson <fetmar@gmail.com>
2016-01-18 16:40:59 -0500

    tweaking install script

--- server-init.sh
+++ server-init.sh
@@ -1,26 +1,43 @@
 # tree
+
 sudo apt-get update
 
-# install JDK and Android SDK
-sudo apt-get install default-jdk -y
-curl http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz > android-sdk.tgz
-tar xvf android-sdk.tgz
-sudo mv android-sdk-linux /usr/local/bin
+# JDK
+which_javac=`which javac`
+if [ ! -z "$which_javac" ]; then
+  echo "JDK already installed"
+else
+  sudo apt-get install default-jdk -y
+fi
 
-# Install Android SDK's tools
-/usr/local/bin/android-sdk-linux/tools/android list sdk --all
-echo "################ Android SDK Install ########################"
-echo "Please select which following packages you wish to install."
-echo "You want the numbers for the following"
-echo "  * Android SDK Tools"
-echo "  * Android SDK Platform Tools"
-echo "  * Android SDK Build Tools"
-echo "  * Android SDK Platform (whatever version you need)"
-echo "For example: 1,2,6,26"
-echo "Install the proper versions please. Example: 1,2,6,26"
-echo -n "Package numbers separated by comma, no spaces:"
-read packages
-/usr/local/bin/android-sdk-linux/tools/android update sdk -u -a -t $packages
+# Android SDK
+if [ ! -z "$which_android" ]
+  echo "Android SDK already installed"
+else
+  curl http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz > android-sdk.tgz
+  tar xvf android-sdk.tgz
+  sudo mv android-sdk-linux /usr/local/bin
+  sudo touch /etc/profile.d/android-sdk-path
+  echo "export PATH=$PATH:/usr/local/bin/android-sdk-linux/tools:/usr/local/bin/android-sdk-linux/platform-tools" > /etc/profile.d/android-sdk-path
+
+
+  # Install Android SDK's tools
+  /usr/local/bin/android-sdk-linux/tools/android list sdk --all
+  echo "################ Android SDK Install ########################"
+  echo "Please select which following packages you wish to install."
+  echo "You want the numbers for the following"
+  echo "  * Android SDK Tools"
+  echo "  * Android SDK Platform Tools"
+  echo "  * Android SDK Build Tools"
+  echo "  * Android SDK Platform (whatever version you need)"
+  echo "For example: 1,2,6,26"
+  echo "Install the proper versions please. Example: 1,2,6,26"
+  echo -n "Package numbers separated by comma, no spaces:"
+  read packages
+  /usr/local/bin/android-sdk-linux/tools/android update sdk -u -a -t $packages
+
+
+fi
 
 
 # node
@@ -35,6 +52,3 @@ sudo git submodule init && git submodule update
 cd client
 sudo npm install
 sudo chown -R www-data:www-data /var/www
-
-
-
