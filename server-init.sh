7227c37582e5861bad1b33ef335eaa2d54804088
Josh Anderson <fetmar@gmail.com>
2016-01-19 00:42:54 -0500

    path fixes and cwd fix

--- server-init.sh
+++ server-init.sh
@@ -15,15 +15,17 @@ else
 fi
 
 # Android SDK
-which_android=`sudo which /usr/local/bin/android-sdk-linux/tools/android`
+which_android=`sudo which android`
 if [ ! -z "$which_android" ]; then
   echo "Android SDK already installed"
 else
   curl http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz > android-sdk.tgz
   tar xvf android-sdk.tgz
   sudo mv android-sdk-linux /usr/local/bin
-  sudo chown -R root:root android-sdk-linux/
-  sudo sh -c "echo \"export PATH=$PATH:/usr/local/bin/android-sdk-linux/tools:/usr/local/bin/android-sdk-linux/platform-tools\" > /etc/profile.d/android-sdk-path.sh"
+  sudo chown -R root:root /usr/local/bin/android-sdk-linux/
+  sudo chmod a+x /usr/local/bin/android-sdk-linux/tools/android
+  export PATH=$PATH:/usr/local/bin/android-sdk-linux/tools:/usr/local/bin/android-sdk-linux/build-tools
+  sudo sh -c "echo \"export PATH=$PATH:/usr/local/bin/android-sdk-linux/tools:/usr/local/bin/android-sdk-linux/build-tools\" > /etc/profile.d/android-sdk-path.sh"
 
   # Install Android SDK's tools
   /usr/local/bin/android-sdk-linux/tools/android list sdk --all
@@ -51,9 +53,12 @@ else
   sudo apt-get install nodejs nodejs-legacy npm -y
 fi
 
+
 git submodule init
 git submodule update
-cd client
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir/client
 npm install
 
 chown -R `whoami` .
